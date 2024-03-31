70688efbd27175fa84cd211daac7de7c663ed130
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-31 19:13:25 -0400

    feat add Now playing, Popular, Top rated, Upcoming hooks

---
+++ hook/popularMovies.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_TOKEN = process.env.API_TOKEN;
+
+const trendingMovies = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    headers: {
+      accept: "application/json",
+      Authorization: API_TOKEN,
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      const image = "https://image.tmdb.org/t/p/original";
+
+      response.data.results.map((item) => {
+        item.poster_path = image + item.poster_path;
+        item.backdrop_path = image + item.backdrop_path;
+      });
+
+      setData(response.data.results);
+
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default trendingMovies;
