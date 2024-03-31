7ecc9f5af7de26191fe127deffc87e89c09ae2ae
Ricardo Alves da Silva <ricardcpu@gmail.com>
2024-03-31 15:23:17 -0400

    feat: add Poppins font

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,20 @@
 import { Stack } from "expo-router/stack";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function AppLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Poppins: require("../assets/fonts/Poppins/Poppins-Regular.ttf"),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
